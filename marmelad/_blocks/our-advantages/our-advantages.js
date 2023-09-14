25d1f5d7dcccc60c61542e2b5b7cab2e6b32ef03
Alex <skyriver.f1@gmail.com>
2023-09-14 06:10:58 +0500

    added our-advantages

---
+++ marmelad/_blocks/our-advantages/our-advantages.js
@@ -0,0 +1,32 @@
+function ourAdvantagesSliderInit() {
+  const ourAdvantagesSlider = new Swiper(".js-our-advantages-slider", {
+    slidesPerView: "auto",
+    // initialSlide: 3,
+    spaceBetween: 47,
+    // centeredSlides: true,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: false,
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        spaceBetween: 20,
+        freeMode: true,
+      },
+      // when window width is >= 641px
+      641: {
+        spaceBetween: 30
+      },
+      // when window width is >= 1281px
+      1281: {
+        spaceBetween: 47,
+        freeMode: false,
+      },
+    }
+  });
+}
+
+if (document.querySelectorAll(".js-our-advantages-slider").length) {
+  ourAdvantagesSliderInit();
+}
